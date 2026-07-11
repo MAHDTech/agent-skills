395bae3fd0f1baf21b199e1f755229074ccdd359
MAHDTech <MAHDTech@saltlabs.tech>
2026-07-11 20:58:06 +1000

    fix: pin deps

--- bin/skills.ts
+++ bin/skills.ts
@@ -1,7 +1,7 @@
 import fs from "fs-extra"
 import path from "path"
 import os from "os"
-import yaml from "js-yaml"
+import * as yaml from "js-yaml"
 import {glob} from "glob"
 import {execFileSync} from "child_process"
 import {intro, outro, log} from "@clack/prompts"
